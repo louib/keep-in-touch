eb55aa0d3f0a1c243be04bccffb7bb941fad221d
louib <code@louib.net>
2022-07-05 13:15:11 -0400

    We will not need subcommands

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,67 @@
 use clap::{AppSettings, Parser, Subcommand};
 
+enum SupportedFormat {
+    KDBX,
+    VCARD,
+}
+impl SupportedFormat {
+    pub fn from_string(format: &str) -> Option<SupportedFormat> {
+        if (format == "kdbx") {
+            return Some(SupportedFormat::KDBX);
+        }
+        if (format == "vcard") {
+            return Some(SupportedFormat::VCARD);
+        }
+        None
+    }
+}
+
 /// CLI tool to translate from kdbx to vcard, and vice versa.
 #[derive(Parser)]
 #[clap(name = "kp2vcard")]
 #[clap(version = env!("CARGO_PKG_VERSION"))]
 #[clap(about = "CLI tool to translate from kdbx to vcard, and vice versa.", long_about = None)]
 struct KP2VCard {
-    #[clap(subcommand)]
-    command: SubCommand,
-}
-
-#[derive(Subcommand)]
-enum SubCommand {
-    /// Formats a Flatpak manifest.
-    #[clap(setting(AppSettings::ArgRequiredElseHelp))]
-    Convert {
-        /// The path of the file to convert.
-        path: String,
-        /// The format to convert to. The extension of the path will be used
-        /// to detect the format if a path is provided.
-        format: Option<String>,
-    },
+    /// The path of the file to convert.
+    path: String,
+    /// The format to convert to. The extension of the path will be used
+    /// to detect the format if a path is provided.
+    format: Option<String>,
 }
 
 fn main() -> std::process::ExitCode {
     let args = KP2VCard::parse();
 
-    match &args.command {
-        SubCommand::Convert { path, format } => {
-            if path.ends_with(".kdbx") {
-                return std::process::ExitCode::SUCCESS;
-            }
+    let file_path = args.path;
 
-            if path.ends_with(".vcard") {
-                return std::process::ExitCode::SUCCESS;
-            }
-
-            eprintln!("Could not detect file format based on path extension.");
-            return std::process::ExitCode::FAILURE;
+    let mut source_format: SupportedFormat = SupportedFormat::KDBX;
+    if let Some(format) = args.format {
+        source_format = match SupportedFormat::from_string(&format) {
+            Some(f) => f,
+            None => {
+                eprintln!("Invalid format {}.", format);
+                return std::process::ExitCode::FAILURE;
+            },
+        };
+    } else {
+        let file_extension = file_path.split(".").last().unwrap();
+        source_format = match SupportedFormat::from_string(&file_extension) {
+            Some(f) => f,
+            None => {
+                eprintln!("Cannot detect file format from extension {}.", file_extension);
+                return std::process::ExitCode::FAILURE;
+            },
         }
     }
+
+    match source_format {
+        SupportedFormat::KDBX => {
+
+        },
+        SupportedFormat::VCARD => {
+
+        },
+    }
+
     std::process::ExitCode::SUCCESS
 }
