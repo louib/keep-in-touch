b17b9a15146287cae680efb09496ba08ca25d388
louib <code@louib.net>
2023-07-08 22:34:26 -0400

    chore(nix): update hash for custom keepass-rs version

--- flake.nix
+++ flake.nix
@@ -50,7 +50,7 @@
                 outputHashes = {
                   # This hash need to be updated everytime you bump the version of the keepass-rs
                   # library.
-                  "keepass-0.6.0-rc.1" = "sha256-HS3jeGx1O/nHW8FpyvEbEwlDLJ+TsdTxyYlvI6/OHTA=";
+                  "keepass-0.6.1" = "sha256-xD/masJ231sm9dXPmSIT6h2k5LD7HTl3+P0WIt+pkB8=";
                 };
               };
 
